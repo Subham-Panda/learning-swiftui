b995ba0a0758271c2ac10e14c91a5efbbdab8f4b
Subham Panda <im.sspanda10@gmail.com>
2022-07-03 23:24:40 +0530

    feat:  WeSplit structure

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Subham Subhasish Panda on 29/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+ 
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Subham Subhasish Panda on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
